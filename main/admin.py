bf5a7b5728813dd21ae96accca57186e06c778e7
AnDISaY <sainirgg@inbox.ru>
2022-01-13 20:44:09 +0600

    All without celery

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Anime, Episode, Genre, Season, Favorites
+from .models import Anime, Episode, Genre, Season, Favorites, Review
 
 admin.site.register(Anime)
 admin.site.register(Season)
 admin.site.register(Genre)
 admin.site.register(Episode)
 admin.site.register(Favorites)
+admin.site.register(Review)
